e1c20a4a8c2fd21288d9b7efb363263e1e726dfe
timnboys <timnboys@users.noreply.github.com>
2018-06-05 16:48:12 -0400

    Update scoreboard.lua

--- scoreboard.lua
+++ scoreboard.lua
@@ -34,35 +34,27 @@ end
                     )
                 end
 				if Faketimer >= 2 then
-				ESX.TriggerServerCallback('stadusrp_getJobsOnline', function(ems, police, taxi, mek, bil, maklare, ica, spelare, airlines, electrician, vigne, epicerie)
+				ESX.TriggerServerCallback('stadusrp_getJobsOnline', function(ems, police, taxi, mek, airlines, electrician, vigne, epicerie)
  myVar2 = ems
  myVar3 = police
  myVar4 = taxi
  myVar5 = mek
- myVar6 = bil
- myVar7 = maklare
- myVar8 = ica
- myVar9 = spelare
- myVar10 = airlines
- myVar11 = electrician
+ myVar6 = airlines
+ myVar7 = electrician
 -- Vigne = Napa Valley
 -- Epicerie = Bar
- myVar12 = vigne
- myVar13 = epicerie
+ myVar8 = vigne
+ myVar9 = epicerie
 
           SendNUIMessage({ text = table.concat(players), 
 				  ems = myVar2,
   police = myVar3,
   taxi = myVar4,
   mek = myVar5,
-  bil = myVar6,
-  maklare = myVar7,
-  ica = myVar8,
-  spelare = myVar9,
-  airlines = myVar10,
-  electrician = myVar11,
-  vigne = myVar12,
-  epicerie = myVar13})
+  airlines = myVar6,
+  electrician = myVar7,
+  vigne = myVar8,
+  epicerie = myVar9})
 	end)
 	Faketimer = 0
 	else
